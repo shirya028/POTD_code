caaba848839be35703663032226bcd881a9e29e8
shirya_028 <shreeyashjanawade@gmail.com>
2024-04-28 00:20:10 +0530

    first commit

---
+++ ComparatorDemo.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+public class ComparatorDemo {
+    public static void main(String[] args) {
+
+        Comparator<ArrayList<Integer>> cmp=new Comparator<ArrayList<Integer>>() {
+            public int compare(ArrayList<Integer> i,ArrayList<Integer> j) {
+                if(i.get(0) > j.get(0))
+                    return 1;
+                return -1;
+            }
+        };
+        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+        ArrayList<Integer> t1=new ArrayList<>();
+        t1.add(3);
+        t1.add(4);
+        list.add(t1); 
+        ArrayList<Integer> t2=new ArrayList<>();
+        t2.add(1);
+        t2.add(3);
+        list.add(t2);
+        ArrayList<Integer> t3=new ArrayList<>();
+        t3.add(4);
+        t3.add(5);
+        list.add(t3);
+        
+        System.out.println(list);
+
+        Collections.sort(list,cmp);
+
+        System.out.println(list);
+    }
+}
