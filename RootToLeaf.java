6c5ddc67bab908b36965bfcd414cafd58750e902
shirya_028 <shreeyashjanawade@gmail.com>
2024-05-08 22:07:35 +0530

    new commit

---
+++ RootToLeaf.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    public static void helper(Node root , ArrayList<ArrayList<Integer>> arr ,ArrayList<Integer> temp) {
+        if(root!=null && root.left==null && root.right==null) {
+            temp.add(root.data);
+            ArrayList<Integer> a=new ArrayList<>();
+            a.addAll(temp);
+            arr.add(a);
+            temp.remove(temp.size()-1);
+            return;
+        }
+        if(root==null)
+            return;
+        temp.add(root.data);
+        helper(root.left,arr,temp);
+        helper(root.right,arr,temp);
+        temp.remove(temp.size()-1);
+        
+    }
+    
+    public static ArrayList<ArrayList<Integer>> Paths(Node root) {
+        ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
+        ArrayList<Integer> temp=new ArrayList<>();
+        helper(root ,arr ,temp);
+        return arr;
+    }
+}
