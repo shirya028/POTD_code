daf8e4cf8717bacd89c93b4ee0e07ff8b3f3bcc8
shirya_028 <shreeyashjanawade@gmail.com>
2024-05-11 23:08:24 +0530

    neww commit

---
+++ JugglerSequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    static List<Long> jugglerSequence(long n) {
+        List<Long> ans=new ArrayList<>();
+        ans.add(n);
+        while(n!=1) {
+            if(n%2==0) {
+                n=(long)Math.pow(n,0.5);
+            }
+            else {
+                n=(long)Math.pow(n,1.5);
+            }
+            ans.add(n);
+        }
+        return ans;
+    }
+  
+}
